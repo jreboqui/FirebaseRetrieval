86a6ba688c09ea5d243ae5c80b7d3bd7f41825fd
Jun Rebo <raijin.dhie@gmail.com>
2017-05-29 10:56:02 -0700

    Added all the imageView files

--- app/src/main/java/com/example/junnel_lalaine/firebaseretrieval/MainActivity.java
+++ app/src/main/java/com/example/junnel_lalaine/firebaseretrieval/MainActivity.java
@@ -1,16 +1,74 @@
 package com.example.junnel_lalaine.firebaseretrieval;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_ID_READ_PERMISSION = 100;
+    private static final int REQUEST_ID_WRITE_PERMISSION = 200;
+
+    private final String fileName = "note.txt";
+    private ImageView imageView;
+
+    private Button saveButton;
+    private EditText editText;
+    private TextView textView;
+
+    private String simpleFileName = "note.txt";
+
+
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,15 +76,254 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //Internal Storage
+        //------------------------
+        this.saveButton = (Button) this.findViewById(R.id.button_save);
+        this.editText = (EditText) this.findViewById(R.id.editText);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View arg0) {
+                askPermissionAndWriteFile();
+            }
+
+        });
+
+
+
+        //one way of making a custom directory?
+        String path = Environment.getExternalStorageDirectory().toString();
+        File dir = new File(path, "/FirebaseRetrieval/media/appimages/");
+        if (!dir.isDirectory()) {
+            dir.mkdirs();
+        }
+
+        //another way of making a directory, actually the same thing
+        File storagePath = new File(Environment.getExternalStorageDirectory(), "/FirebaseRetrieval/");
+        // Create direcorty if not exists
+        if(!storagePath.exists()) {
+            storagePath.mkdirs();
+        }
+
+
+        imageView = (ImageView) findViewById(R.id.image);
+        //Firebase storage link method https://www.youtube.com/watch?v=7bFiVTpNrl4&ab_channel=JameelAhamed
+     //   String url = "https://firebasestorage.googleapis.com/v0/b/imagebase-1d29d.appspot.com/o/beach.jpg?alt=media&token=eb13f524-ad09-49c4-99fa-a98b579f581a";
+        //    storageRef = FirebaseStorage.getInstance().getReference();
+        //from glide, load it to imageView
+        //  Glide.with(getApplicationContext()).load(url).into(imageView);
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReferenceFromUrl("gs://imagebase-1d29d.appspot.com").child("beach.jpg");
+
+        //instead of using glide, let's bind it to a file and use it
+
+
+
+           /*
+            //scan the image so show up in album
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            //  File f = new File(dir, imagePath);
+            Uri contentUri = Uri.fromFile(somefile);
+            mediaScanIntent.setData(contentUri);
+            this.sendBroadcast(mediaScanIntent);
+            */
+        try {
+           final File localFile = File.createTempFile("images", "jpg");
+          //final File myFile = new File(dir,"file_name");
+
+          storageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                imageView.setImageBitmap(bitmap);
+                /*
+                try {
+                    saveImageToExternal("newImage", bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                */
+
+            }
+
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
             }
         });
+        } catch (IOException e ) {}
+
+        //-------------------------------------------------------------------
+        //Insert to Internal Storage
+        //
+        //
+        //--------------------------------------------------------------------
+
+
+
+
     }
+    //---------------------------------------------------------------------
+    //
+    //Insert to Internal method
+    //
+    //---------------------------------------------------------------------
+    private void saveData() {
+        String data = this.editText.getText().toString();
+        try {
+
+
+            // Open Stream to write file.
+            FileOutputStream out = this.openFileOutput(simpleFileName, MODE_PRIVATE);
+
+            out.write(data.getBytes());
+            out.close();
+            Toast.makeText(this,"File saved!",Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this,"Error:"+ e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //---------------------------------------------------------------------
+    //
+    //Insert to External method
+    //
+    //---------------------------------------------------------------------
+    private void askPermissionAndWriteFile() {
+        boolean canWrite = this.askPermission(REQUEST_ID_WRITE_PERMISSION,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        //
+        if (canWrite) {
+            this.writeFile();
+        }
+    }
+
+    // With Android Level >= 23, you have to ask the user
+    // for permission with device (For example read/write data on the device).
+    private boolean askPermission(int requestId, String permissionName) {
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+
+            // Check if we have permission
+            int permission = ActivityCompat.checkSelfPermission(this, permissionName);
+
+
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // If don't have permission so prompt the user.
+                this.requestPermissions(
+                        new String[]{permissionName},
+                        requestId
+                );
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    // When you have the request results
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //
+        // Note: If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0) {
+            switch (requestCode) {
+                case REQUEST_ID_READ_PERMISSION: {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        readFile();
+                    }
+                }
+                case REQUEST_ID_WRITE_PERMISSION: {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        writeFile();
+                    }
+                }
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Permission Cancelled!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void writeFile() {
+
+        File extStore = Environment.getExternalStorageDirectory();
+        // ==> /storage/emulated/0/note.txt
+        String path = extStore.getAbsolutePath() + "/" + fileName;
+        Log.i("ExternalStorageDemo", "Save to: " + path);
+
+        String data = editText.getText().toString();
+
+        try {
+            File myFile = new File(path);
+            myFile.createNewFile();
+            FileOutputStream fOut = new FileOutputStream(myFile);
+            OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+            myOutWriter.append(data);
+            myOutWriter.close();
+            fOut.close();
+
+            Toast.makeText(getApplicationContext(), fileName + " saved", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readFile() {
+
+        File extStore = Environment.getExternalStorageDirectory();
+        // ==> /storage/emulated/0/note.txt
+        String path = extStore.getAbsolutePath() + "/" + fileName;
+        Log.i("ExternalStorageDemo", "Read file: " + path);
+
+        String s = "";
+        String fileContent = "";
+        try {
+            File myFile = new File(path);
+            FileInputStream fIn = new FileInputStream(myFile);
+            BufferedReader myReader = new BufferedReader(
+                    new InputStreamReader(fIn));
+
+            while ((s = myReader.readLine()) != null) {
+                fileContent += s + "\n";
+            }
+            myReader.close();
+
+            this.textView.setText(fileContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(getApplicationContext(), fileContent, Toast.LENGTH_LONG).show();
+    }
+    /*
+    public void saveImageToExternal(String imgName, Bitmap bm) throws IOException {
+        //Create Path to save Image
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES); //Creates app specific folder
+        path.mkdirs();
+        File imageFile = new File(path, imgName+".png"); // Imagename.png
+        FileOutputStream out = new FileOutputStream(imageFile);
+        try{
+            bm.compress(Bitmap.CompressFormat.PNG, 100, out); // Compress Image
+            out.flush();
+            out.close();
+
+            // Tell the media scanner about the new file so that it is
+            // immediately available to the user.
+            MediaScannerConnection.scanFile(this,new String[] { imageFile.getAbsolutePath() }, null,new MediaScannerConnection.OnScanCompletedListener() {
+                public void onScanCompleted(String path, Uri uri) {
+                    Log.i("ExternalStorage", "Scanned " + path + ":");
+                    Log.i("ExternalStorage", "-> uri=" + uri);
+                }
+            });
+        } catch(Exception e) {
+            throw new IOException();
+        }
+    }
+    */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
